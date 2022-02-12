8b5b265678d42287e664ce47e0b5d269575a3839
himanshuc11 <himanshuchhatpar@gmail.com>
2022-02-12 19:29:49 +0530

    Deployment

--- Blog/settings.py
+++ Blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
